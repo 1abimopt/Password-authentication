e5a3e9d2cc120741240dc308ba1332a99c235561
Bims <aaabimss@gmail.com>
2021-01-27 18:10:42 +0100

    Change password to Integer

--- Password Authentication.py
+++ Password Authentication.py
@@ -2,10 +2,13 @@ print('Welcome to IHS towers')
 answer = input("Are you an IHS staff? yes/no")
 
 if answer == 'yes':
-    password = input('Please enter your password?')
-    if password == 'glory':
+    password = int(input('Please enter your password?'))
+    if password == 2021:
         print('Access granted!')
     else:
         print('Wrong password')
 else:
     print('Access denied!')
+
+
+
